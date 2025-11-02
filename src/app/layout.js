65c0ed813ceb6c6cc886dba146a130d22d199268
pravindev666 <pravindev666@gmail.com>
2025-11-02 07:01:25 +0530

    Initial commit - portfolio setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9MFJS30727"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9MFJS30727');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
