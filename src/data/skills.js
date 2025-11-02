3911486f2fd924e9baa7fc7e64fc754d80c336d0
pravindev666 <pravindev666@gmail.com>
2025-11-02 05:59:56 +0530

    Initial commit

---
+++ src/data/skills.js
@@ -0,0 +1,33 @@
+export const skills = {
+  cloud: [
+    { name: 'AWS' },
+    { name: 'Netlify' }
+  ],
+  development: [
+    { name: 'Python' },
+    { name: 'MongoDB' },
+    { name: 'MySQL' },
+    { name: 'GitHub' },
+    { name: 'Jira' },
+    { name: 'HTML' },
+    { name: 'CSS' },
+    { name: 'Postman' }
+  ],
+  devops: [
+    { name: 'Linux' },
+    { name: 'Git' },
+    { name: 'Splunk' },
+    { name: 'Prometheus' },
+    { name: 'Grafana' },
+    { name: 'GitHub Actions' },
+    { name: 'Docker' },
+    { name: 'Kubernetes' },
+    { name: 'Terraform' },
+    { name: 'Jenkins' },
+    { name: 'Ansible' },
+    { name: 'Bash' },
+    { name: 'Powershell' },
+    { name: 'YAML' },
+    { name: 'Nginx' }
+  ]
+};
